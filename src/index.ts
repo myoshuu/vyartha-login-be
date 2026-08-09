dc2187066c6404dc6e804e1091915865af2ead03
myoshuu <myoshu.me@gmail.com>
2026-08-10 01:54:40 +0700

    fix: idk what im doing

--- src/index.ts
+++ src/index.ts
@@ -183,15 +183,7 @@ app.post('/player/growid/login/validate', async ({ request }) => {
         `_token=${_token}&growId=${growId}&password=${password}&reg=1`,
       ).toString('base64');
 
-      return new Response(JSON.stringify({
-        status: 'success',
-        message: 'Account created successfully.',
-        token,
-        url: '',
-        accountType: 'growtopia',
-      }), {
-        headers: { 'Content-Type': 'application/json' },
-      });
+      return Response.redirect(`https://login.growtopiagame.com/player/validate/close?token=${encodeURIComponent(token)}`, 302);
     }
 
     // @note Login flow (no email field)
@@ -222,15 +214,7 @@ app.post('/player/growid/login/validate', async ({ request }) => {
       `_token=${_token}&growId=${growId}&password=${password}&reg=0`,
     ).toString('base64');
 
-    return new Response(JSON.stringify({
-      status: 'success',
-      message: 'Account Validated.',
-      token,
-      url: '',
-      accountType: 'growtopia',
-    }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return Response.redirect(`https://login.growtopiagame.com/player/validate/close?token=${encodeURIComponent(token)}`, 302);
   } catch (error) {
     console.log(`[ERROR]: ${error}`);
     return new Response(JSON.stringify({ status: 'error', message: 'Internal Server Error' }), {
